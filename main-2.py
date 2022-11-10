7a6f935494e07504f1e962c9a845f0a292bb1a7f
Rafael Queiroz <wrafael.queiroz@icloud.com>
2022-11-10 20:30:41 +0400

    Add files via upload

---
+++ main-2.py
@@ -0,0 +1,44 @@
+import random
+import hangman_art
+from hangman_words import word_list
+
+chosen_word = random.choice(word_list)
+display = []
+guessed_letters = []
+game_over = False
+lives = 6
+
+print(f'{hangman_art.logo}' + "\n\n\n")
+
+for letter in chosen_word:
+  display += "_"
+print(f"{' '.join(display)}\n")
+print(f"The word has {len(display)} letters. What is your guess?\n")
+
+while not game_over:
+    guess = input("\nPick a letter: ").lower()
+   
+    for position in range (len(chosen_word)):
+      letter = chosen_word[position]
+      if letter == guess:
+        display[position] = letter
+    print(f"{' '.join(display)}")
+    
+    if guess not in chosen_word:
+      if guess in guessed_letters:
+          print(f"\nYou've already selected the letter {guess} before. Try again\n")
+          print(f"{' '.join(display)}")
+      else:  
+          lives -= 1
+          stage = lives + 1
+          guessed_letters += guess
+          print (f"\nThe letter {guess} is not in the chosen word. Better luck next time. Lives remining {stage}")
+          print (hangman_art.stages[stage])
+          print(f"{' '.join(display)}")
+    
+    if "_" not in display or lives < 0:
+        game_over = True
+        if "_" not in display:
+              print("You Win")
+        else:
+              print("You Lose")
